8a0890d10928b82e77ca6c10e80aabdb120251fe
Eduardo <eduardooliveira7907@gmail.com>
2025-10-15 14:54:56 -0300

    criando métodos das classes Cadastrosdedisciplinas  e historico de notas

--- src/CadastrodeDisciplinas.java
+++ src/CadastrodeDisciplinas.java
@@ -6,17 +6,32 @@ public class CadastrodeDisciplinas {
     CadastrodeDisciplinas (String nome, String codigo){
         this.codigo = codigo;
         this.nome = nome;
+        this.disciplinas = new ArrayList<>();
     }
     public void adicionarDisciplina(Disciplina disciplina){
-        this.disciplinas.add(disciplina);
+        if(!verificarDisciplina(disciplina.codigo)) {
+            this.disciplinas.add(disciplina);
+        }
     }
-    public void verificarDisciplina(String codigo){
-
+    public boolean verificarDisciplina(String codigo){
+        for (Disciplina disciplina1 : this.disciplinas) {
+            if (disciplina1.codigo.toLowerCase().equals(codigo.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
     }
     public void removerDisciplina(String codigo){
+        for (Disciplina disciplina1 : this.disciplinas) {
+            if(disciplina1.equals(codigo)) {
+                this.disciplinas.remove(disciplina1);
+                break;
+              }
+            }
 
     }
-    public void obterTodasDisciplinas() {
+    public ArrayList<Disciplina> obterTodasDisciplinas() {
+        return this.disciplinas;
 
     }
 
--- src/HistoricoNotas.java
+++ src/HistoricoNotas.java
@@ -1,27 +1,46 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
 public class HistoricoNotas {
-    String nome;
-    int id;
-    String codigoDisciplina;
-    double nota;
-    public HistoricoNotas(String nome, int id, String codigoDisciplina, double nota){
-        this.codigoDisciplina = codigoDisciplina;
-        this.id = id;
-        this.nome = nome;
-        this.nota = nota;
-    }
-    public void adicionarMatricula(int id, String codigoDisciplina, double nota){
+     HashMap <Integer , ArrayList<Matricula>> historicoCompleto;
+
+    public HistoricoNotas(){
+        this.historicoCompleto = new HashMap<>();
 
     }
-    public void obterMatriculas(int id){
-
+    public void adicionarMatricula (int idEstudante, String codigoDisciplina, double nota) {
+        Matricula novaMatricula = new Matricula(codigoDisciplina, nota);
+        if (historicoCompleto.containsKey(idEstudante)) {
+            historicoCompleto.get(idEstudante).add(novaMatricula);
+        } else {
+            ArrayList<Matricula> novaLista = new ArrayList<>();
+            novaLista.add(novaMatricula);
+            historicoCompleto.put(idEstudante, novaLista);
+        }
     }
-    public void obterNota(int id, String codigoDisciplina){
 
-    }
-    public void removerMatricula(int id, String codigoDisciplina){
+    public ArrayList<Matricula> obterMatriculas (int idEstudante){
+            if (historicoCompleto.containsKey(idEstudante)) {
+                return historicoCompleto.get(idEstudante);
+            } else {
+                return new ArrayList<>();
+            }
+        }
 
-    }
+    public ArrayList<HistoricoNotas> obternotas (int idEstudante, String codigoDisciplina) {
+
+        }
+
+    public Matricula RemoverMatricula (int idEstudante, String codigoDisciplina, double nota) {
+            if(historicoCompleto.containsKey(idEstudante)) {
+                historicoCompleto.get(idEstudante).remove(Matricula);
+            } else {
+                System.out.println("n tem essa matricula");
+            }
+
     public void MediaDoEstudante(int id){
+    // soma das notas / numero de notas = media das notas
 
     }
     public void MediaDaDisciplina(String codigoDisciplina){
@@ -30,5 +49,5 @@ public class HistoricoNotas {
     public void topNEstudantesPOrMedia(int N){
 
     }
-}
+
 
